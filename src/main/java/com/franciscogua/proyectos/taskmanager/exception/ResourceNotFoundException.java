cb7f4714fe2cc506db108a474ca9c614f54e652c
franciscogua <franguaful@gmail.com>
2025-06-25 13:21:55 -0300

    refactor(exception): Add @ResponseStatus to custom exceptions

--- src/main/java/com/franciscogua/proyectos/taskmanager/exception/ResourceNotFoundException.java
+++ src/main/java/com/franciscogua/proyectos/taskmanager/exception/ResourceNotFoundException.java
@@ -1,9 +1,13 @@
 package com.franciscogua.proyectos.taskmanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  *
  * @author Francisco
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     
     public ResourceNotFoundException(String message) {
