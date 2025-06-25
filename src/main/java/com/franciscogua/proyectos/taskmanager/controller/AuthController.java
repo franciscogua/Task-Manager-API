3e24667894317dd4e0357bf4f24f18d512cbc9ae
franciscogua <franguaful@gmail.com>
2025-06-25 13:05:34 -0300

    feat: Add AuthController with user registration endpoint

---
+++ src/main/java/com/franciscogua/proyectos/taskmanager/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.franciscogua.proyectos.taskmanager.controller;
+
+import com.franciscogua.proyectos.taskmanager.dto.UserCreateDTO;
+import com.franciscogua.proyectos.taskmanager.dto.UserResponseDTO;
+import com.franciscogua.proyectos.taskmanager.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Francisco
+ */
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody UserCreateDTO userCreateDTO) {
+        
+        UserResponseDTO createdUser = userService.createUser(userCreateDTO);
+        
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
